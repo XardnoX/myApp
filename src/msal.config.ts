25702b7bb80e00eca690f8cf496cca452563f16a
XardnoX <ondra.kraja@gmail.com>
2025-01-12 01:28:02 +0100

    bug fixes

---
+++ src/msal.config.ts
@@ -0,0 +1,7 @@
+export const msalConfig = {
+  auth: {
+    clientId: "8117fe5b-a44f-42a2-89c3-f40bc9076356", // Replace with your Azure client ID
+    authority: "https://login.microsoftonline.com/common", // For multi-tenant apps
+    redirectUri: "msauth://MyApp/2jmj7l5rSw0yVb%2FvlWAYKk%2FYBWk%3D", // Update with your redirect URI
+  },
+};
